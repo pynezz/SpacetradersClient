dacb3ad5f3e9fd04b374c9031a328a4d1294592c
pynezz <kadam@pm.me>
2023-06-27 18:26:11 +0200

    Added contracts WIP

--- src/lib/components/Cards/Card.svelte
+++ src/lib/components/Cards/Card.svelte
@@ -7,7 +7,7 @@
     <TextRow text="{objects[0]}" fontSize="text-lg" />
     {#each objects as object}
         {#if object[0] != objects[0]}
-            <TextRow text="{object[1]}" fontSize="text-md" />
+            <TextRow text="{object[0]}" fontSize="text-md" />
         {/if}
     {/each}
 </div>
---
+++ src/lib/components/contracts/Contracts.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    export let contracts: Array<any> = [];
+</script>
+
+<div class="flex">
+
+    {#each contracts as contract (contract.id)}
+    <div class="text-green-500">
+
+    </div>
+
+    {/each}
+</div>
